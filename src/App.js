d0fc6746f519ebab5195e46ee988fc3d08fe250d
Vipul <vipuldeep95@gmail.com>
2021-01-24 21:11:04 +1000

    resuming project

--- src/App.js
+++ src/App.js
@@ -18,10 +18,10 @@ const app = new Clarifai.App({
 const particlesOptions ={
   particles: {
     number:{
-      value: 80,
+      value: 0,
       density:{
         enable: true,
-        value_area: 800
+        value_area: 0
       }
     }
   }
